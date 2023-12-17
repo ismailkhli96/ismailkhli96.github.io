beac6a3e0dffe7d6a8b4af5cae56677d2ca22dbb
ismailkhli96 <107446877+ismailkhli96@users.noreply.github.com>
2023-12-17 22:55:24 +0100

    Update ldap.php

--- ldap.php
+++ ldap.php
@@ -1,40 +1,6 @@
 <?php
 session_start();
 header("Location: search.php");
-        exit();
-// LDAP-Konfiguration
-/*//$ldapServer = "ldap://your-ldap-server";
-$ldapBaseDN = "ou=Users,dc=example,dc=com";*///
+exit();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    //$ldapconn = ldap_connect($ldapServer);
-
-    if ($username=="admin" & $password=="admin" ){
-        $_SESSION["username"] = $username;
-        header("Location: search.php");
-        exit();
-    }
-
-   /* if ($ldapconn) {
-        ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-        ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
-
-        $bind = @ldap_bind($ldapconn, "uid=$username,$ldapBaseDN", $password);
-
-        if ($bind) {
-            $_SESSION["username"] = $username;
-            header("Location: search.php");
-            exit();
-        } else {
-            echo "Ungültige Anmeldedaten. Bitte versuchen Sie es erneut.";
-        }
-
-        ldap_close($ldapconn);
-    } else {
-        echo "LDAP-Verbindung fehlgeschlagen.";
-    }*/
-}
 ?>
